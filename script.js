b2629f3cb46d31243602a35f7ac02b8dcc637b70
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-27 12:45:20 +0100

    added styles

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const btn = document.querySelector("button")
 let number = 1
 const addElement = () => {
     const div = document.createElement("div")
-    div.textContent = number
+    div.innerHTML = number
+    if(number%5 == 0) {
+        div.classList.add("circle")
+    }
+    if(number%4 == 0) {
+        div.classList.add("red")
+    }
+    if(number%3 == 0) {
+        div.classList.add("green")
+    }
+    if(number%2 == 0) {
+        div.classList.add("big")
+    }
     document.body.appendChild(div)
-    number += 1
+    number ++
 }
 btn.addEventListener("click", addElement)
